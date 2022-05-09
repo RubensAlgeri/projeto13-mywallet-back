d386b2646b69f72a1ecec4acb9bccb86c72a4920
Rubens Algeri <rubens_zys@hotmail.com>
2022-05-09 01:35:42 -0300

    melhorar legibilidade

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,12 +2,14 @@ import bcrypt from 'bcrypt';
 import { v4 as uuid } from 'uuid';
 import db from '../db.js';
 import { stripHtml } from "string-strip-html"
+import dotenv from "dotenv"
+dotenv.config()
 
 export async function signUp(req, res) {
     const {password} = req.body;
     const username = stripHtml(req.body.username).result.trim();
     const email = stripHtml(req.body.email).result.trim();
-    const passwordHash = bcrypt.hashSync(password, 10);
+    const passwordHash = bcrypt.hashSync(password, parseInt(process.env.HASH));
 
     await db.collection('users').insertOne({username, email, password: passwordHash })
 
