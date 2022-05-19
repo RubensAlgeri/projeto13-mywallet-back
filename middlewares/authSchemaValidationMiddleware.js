274f93156802b2998eff8bc9937b303b5842f91c
Rodrigodcs <rodrigodcsee@gmail.com>
2022-05-19 16:04:37 -0300

    Preparando revisão de código

--- middlewares/authSchemaValidationMiddleware.js
+++
@@ -1,9 +0,0 @@
-import userSchema from "../schemas/authSchema.js";
-
-export function validaUsuario(req, res, next) {
-const validation = userSchema.validate(req.body);
-if (validation.error) {
-    return res.status(422).send(validation.error.details.map(details=>details.message));
-}
-next()
-}
